8db016f6ede18be908e689d5da77ef412999feab
OLINGERDM22 <OlingerDM22@gcc.edu>
2023-04-18 11:47:18 -0400

    Removed the Package line thingy from each class
    Added stubs for most variables and methods in PlayerCharacter

--- src/InventoryItem.java
+++ src/InventoryItem.java
@@ -1,2 +1,2 @@
-package PACKAGE_NAME;public class InventoryItem {
+public class InventoryItem {
 }
--- src/PlayerCharacter.java
+++ src/PlayerCharacter.java
@@ -1,2 +1,65 @@
-package PACKAGE_NAME;public class PlayerCharacter {
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class PlayerCharacter {
+    private HashMap<String, Integer> abilityScores = new HashMap(6,75);
+    //hashmap of spells needed
+    private ArrayList<InventoryItem> inventory = new ArrayList<>();
+
+
+    private int maxHealth;
+    private int currentHealth;
+    private int gold;
+    private int armorClass;
+    private int speed;
+
+    private int[] deathSave;
+
+
+
+
+
+    public PlayerCharacter(){
+
+    }
+    public PlayerCharacter(String fileName){
+
+    }
+    public int getMod(){
+        return -1; // stub
+    }
+//    skill/saving throw checks
+//    failDS
+//            succDS
+//    clearDS
+//    attack(Weapon)returns damage
+//    addItem/removeItem
+//            rollAbilityCheck
+//    rollDie(Number, Sides,  Bonus)
+//+castSpell(Spell)
+    public void castSpell(Spell spell){
+
+    }
+//+getGold
+    public int getGold() {
+        return -1; //stub
+    }
+
+//+changeGold(value)
+    public void changeGold(){
+
+    }
+//+printSheet()
+    public void printSheet(){
+
+    }
+//+longRest()
+    public void longRest(){
+
+    }
+//+shortRest(numHitDice)
+
+
+
+
 }

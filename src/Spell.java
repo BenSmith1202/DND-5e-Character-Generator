ed6eaf21f54942b5026540a06bbe2ceaf5eaebee
OLINGERDM22 <OlingerDM22@gcc.edu>
2023-04-19 13:05:19 -0400

    Added stubs for Spell variables and Constructor

--- src/Spell.java
+++ src/Spell.java
@@ -1,12 +1,14 @@
 public class Spell {
 
   //hopefully this pushes now
-  private String name;
-    private int damage;
-    private int slotLevel;
+    private String name;
+  private int damage;
+  private int slotLevel;
 
-    public Spell(String name, int damage, int slotLevel){
+  public Spell(String name, int damage, int slotLevel){
 
-    }
+  }
+
+
 
 }

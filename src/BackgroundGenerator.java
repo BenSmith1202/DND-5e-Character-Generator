1908325f3437e4b0c83bbc6cbd691ecf84aac6a2
BenSmith1202 <smithba22@gcc.edu>
2023-05-02 19:51:02 -0400

    Added Skeleton for a backstory generator class.

---
+++ src/BackgroundGenerator.java
@@ -0,0 +1,140 @@
+public class BackgroundGenerator {
+    private static final String[] backgrounds = new String[]{ //Backgrounds from http://dnd5e.wikidot.com
+            "Common Backgrounds",
+            "Setting Specific",
+            "Acolyte",
+            "Anthropologist",
+            "Archaeologist",
+            "Athlete",
+            "Charlatan",
+            "City Watch",
+            "Clan Crafter",
+            "Cloistered Scholar",
+            "Courtier",
+            "Criminal",
+            "Entertainer",
+            "Faceless",
+            "Faction Agent",
+            "Far Traveler",
+            "Feylost",
+            "Fisher",
+            "Folk Hero",
+            "Gladiator",
+            "Guild Artisan",
+            "Guild Merchant",
+            "Haunted One",
+            "Hermit",
+            "House Agent",
+            "Inheritor",
+            "Investigator",
+            "Knight",
+            "Knight of the Order",
+            "Marine",
+            "Mercenary Veteran",
+            "Noble",
+            "Outlander",
+            "Pirate",
+            "Sage",
+            "Sailor",
+            "Shipwright",
+            "Smuggler",
+            "Soldier",
+            "Spy",
+            "Urban Bounty Hunter",
+            "Urchin",
+            "Caravan Specialist",
+            "Harborfolk",
+            "Gate Urchin",
+            "Secret Identity",
+            "Shade Fanatic",
+            "Trade Sheriff",
+            "Gate Warden",
+            "Mage of High Sorcery",
+            "Rune Carver",
+            "Failed Merchant",
+            "Gambler",
+            "Plaintiff",
+            "Rival Intern",
+            "Dissenter",
+            "Initiate",
+            "Vizier",
+            "Archival Assistant",
+    };
+
+    private static final String[] location1 = new String[]{
+
+    };
+    private static final String[] location2 = new String[]{
+
+    };
+    private static final String[] adjective1 = new String[]{
+
+    };
+    private static final String[] wasInSituation = new String[]{
+
+    };
+    private static final String[] somethingHappenedToMe = new String[]{
+
+    };
+    private static final String[] occupationModifier = new String[]{
+
+    };
+    private static final String[] didSomethingBad = new String[]{
+
+    };
+    private static final String[] doSomethingHeroic = new String[]{
+
+    };
+    private static final String[] verbing = new String[]{
+
+    };
+    private static final String[] relatives = new String[]{
+
+    };
+    private static final String[] aggressiveVerbed = new String[]{
+
+    };
+    private static final String[] hostileParty = new String[]{
+
+    };
+    private static final String[] randomObject = new String[]{
+
+    };
+    private static final String[] randomAbility = new String[]{
+
+    };
+    private static final String[] mysterySite = new String[]{
+
+    };
+    private static final String[] mysteryEntity = new String[]{
+
+    };
+
+    //intro 1
+    //Growing up in [Location1] was [Adjective1]. I [wasInSituation1], which meant that [Something happened to me].
+    //These circumstances led me to life as a(n) [Background], but nothing lasts forever.
+
+    //intro 2
+    //I wasn't always a(n) [Class], I grew up as a(n) [occupation modifier][Background] in [Location1]. After I
+    //[didSomethingBad], they fired me, which led to my current occupation. But being a [Background] wasn't my final destiny.
+
+    //intro 3
+    //They should write songs about [Class]s like me. I may have started my life in [Location1] as a [Background], but I always dreamt I
+    //would [doSomethingHeroic] and earn the respect of everyone. Unfortunately, the gods had other plans.
+
+    //Call To Adventure 1
+    //One night in [Location1], while I was [verbing] with my [relatives], we were [agressiveVerbed] by a [hostileParty]. After the tradgedy,
+    //I swore to never let another person be [aggressive verbed] again, and so I took up arms as an adventurer.
+
+    //Call To Adventure 2
+    //Being a [Background] doesn't pay enough for my taste, so when I overheard something about high-paying job
+    //while [verbing] in [Location 2], I was interested. All I needed was my trusty [Object] and my [random ability] to
+    //complete the quest, make sure the [Hostile Party] was [agressiveVerbed], and claim my coin. And that, is how I became an adventurer.
+
+    //Call To Adventure 3
+    //One day, after a long night of drinking, some of my [relatives], my friend [getName] and I were exploring an abandoned
+    // [mysterySite] near [Location2], but I was separated from them at some point, and found myself standing in front of a [MysteryEntity].
+    //I could feel its voice stirring something within me, reminding me of my ideals and strengthening my resolve. If I
+    //wanted to achieve my dreams as a [Class], I couldn't go [verbing] around any longer. I needed to become an adventurer.
+
+}
